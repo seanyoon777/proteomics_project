ab594f947859f9290272844de0971bf16853d2b0
Sean Yoon <76602593+seanyoon777@users.noreply.github.com>
2023-06-04 21:25:56 -0700

    Update enrichAnalysis.R

--- Scripts/Helpers/enrichAnalysis.R
+++ Scripts/Helpers/enrichAnalysis.R
@@ -8,3 +8,12 @@ enrichVolcanodata <- function(data, db) {
   enriched <- enrichr(str_extract(data$Protein, "\\w+(?=\\.)"), db)
   return(enriched)
 }
+
+enrichClusts <- function(clustdata) {
+  enriched <- list()
+  for(i in 1:length(unique(clustdata$cluster))) {
+    clustproteins <- clustdata[clustdata$cluster == i, ]$protein
+    enriched[[i]] <- enrichr(str_extract(clustproteins, "\\w+(?=\\.)"), "ChEA_2022")$ChEA_2022
+  }
+  return(enriched)
+}
